038f5ddc273aaf07cf8571e4e56baedb9481f838
SteveGnv <steve.blay@advancedpokertraining.com>
2014-02-17 23:44:06 -0500

    Now this is price_list too

--- price_list.php
+++ price_list.php
@@ -9,7 +9,7 @@
 <h2>Money Back Guarantee</h2>
 <p>First and most importantly, <strong>our site provides you access to a personalizable NL Holdem Poker game that does not involve gambling.  Our game is solely for entertainment and training purposes.</strong></p>
 <p>It is <strong>free</strong> to try out our game with basic features.</p>
-<p> We feel confident that you will enjoy the game. Practicing your poker using our patented training tools is both fun and a <a href="research.php">proven way</a> of strengthening your poker skills.  Our method is tested, and if you apply it for 1 month, it WILL WORK.  If, however, you are not ABSOLUTELY CONVINCED that your poker skills have improved, simply cancel your membership, ask for a refund and we will refund your first month of membership with no hassles.  Your refund will be processed back to your original payment method. </p>
+<p> We feel confident that you will enjoy the game. Practicing your poker using our patented training tools is both fun and a <a href="research.php">proven way</a> of strengthening your poker skills.  Our method is tested, and if you apply it for 1 month, it WILL WORK.  If, however, this is a test you are not ABSOLUTELY CONVINCED that your poker skills have improved, simply cancel your membership, ask for a refund and we will refund your first month of membership with no hassles.  Your refund will be processed back to your original payment method. </p>
 <p>To cancel your membership at any time, a link to cancel is on your <a href="member.php">membership page</a> under "Additional Links".</p>
 <p>If you have any questions, please contact us via our <a href="contact.php">contact page</a>.</p>
 <?php include('footer.php'); ?>
