ff09cf4cbe062da399639d9565afdb08a309b44a
SteveGnv <steve.blay@advancedpokertraining.com>
2014-02-17 23:40:25 -0500

    Initial Check in
    This is a test

---
+++ privacy_policy.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+$title = "Privacy Policy - Advanced Poker Training";
+$description = "This is the privacy policy for AdvancedPokerTraining.com, home of a unique NL Holdem Poker Training Game";
+$keywords = "Privacy Policy";
+include('header.php'); ?>
+
+<h2>We Value Your Privacy</h2>
+
+<p>Simply put, our privacy policy is as follows: </p>
+<p><strong>"We value your privacy and will NEVER sell your personal information to ANYONE for ANY REASON. We only collect the information we need to provide you with our product, and we will guard your personal information with utmost security. Your personal information will only be stored on SSL secured servers and we will never store your personal information in hard copy form.</strong>"</p>
+<p>Specifically:</p>
+<p>For each visitor to our Web page, our Web server automatically collects <strong>no </strong>information regarding your domain or e-mail address, except to prevent the abuse of our site or sharing of memberships.</p>
+<p>We collect aggregate information on what pages consumers access or visit, information volunteered by the consumer, such as survey information and/or site registrations.</p>
+<p>With respect to cookies: We use cookies to ensure the appropriate functioning of the game. We do not use cookies to collect personal information.</p>
+<p>If you do not want to receive e-mail from us, just let us know. We limit email contact to information directly related to your purpose for registering at our site, such as providing you with login information, information about the products you have purchased, receipts, news about our site or poker, and other information related directly to your activities on our site.</p>
+<p>If you supply us with your postal address on-line we will only use it only for the purpose of delivering to you a product you requested. We do not distribute it to anyone else for any reason.</p>
+<p>Upon request we provide site visitors with access to unique identifier information (e.g., customer number or password) that we maintain about them, transaction information (e.g., dates on which customers made purchases, amounts and types of purchases) that we maintain about them, contact information (e.g., name, address, phone number) that we maintain about them , and a description of information that we maintain about them.</p>
+<p>Customers can access this information by contacting us or through their training page.</p>
+<p>With respect to security: We have appropriate security measures in place in our physical facilities to protect against the loss, misuse or alteration of information that we have collected from you at our site. All information we collect from you is stored on secure servers, and never in hard copy.</p>
+<p>If you feel that this site is not following its stated information policy, you may contact The DMA's Committee on Ethical Business Practices at <a href="mailto:mgoldberger@the-dma.org" class="h4">mgoldberger@the-dma.org</a>, state or local chapters of the Better Business Bureau, state or local consumer protection office or just contact us and we will be happy to discuss it with you.  We value your privacy and data security.</p>
+
+<?php include('footer.php'); ?>
